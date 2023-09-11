edfd9236fb03ee8a880851d5b627bc8cb93e475a
Caioscg <cacazaok5@gmail.com>
2023-09-11 20:14:51 -0300

    correcting inputs size

--- src/pages/admin/AddMeal/index.jsx
+++ src/pages/admin/AddMeal/index.jsx
@@ -4,6 +4,8 @@ import { GoBack } from "../../../components/GoBack";
 import { AdminHeader } from "../../../components/AdminHeader";
 import { Footer } from "../../../components/footer";
 
+import { FiUpload } from "react-icons/fi"
+
 export function AddMeal() {
     return(
         <Container>
@@ -15,30 +17,33 @@ export function AddMeal() {
 
                 <h1>Adicionar prato</h1>
                 <div className="first">
-                    <div className="image">
-                        <label>Imagem do prato</label>
-                        <input type="file" />
-                    </div>
-                    <div className="name">
+                    <label className="normal" htmlFor="food">Imagem do prato
+                        <p>
+                            <FiUpload size={24}/>
+                            Selecione imagem
+                        </p>
+                        <input type="file" id="food"/>
+                    </label>
+                    <div className="nome normal">
                         <label>Nome</label>
                         <input type="text" placeholder="Ex.: Salada Ceasar"/>
                     </div>
-                    <div className="categoria">
+                    <div className="categoria normal">
                         <label>Categoria</label>
                         <input type="text" placeholder="Refeição"/>
                     </div>
                 </div>
                 <div className="second">
-                    <div className="ingredientes">
+                    <div className="ingredientes normal">
                         <label>Ingredientes</label>
                         <input type="text" placeholder="Refeição"/>
                     </div>
-                    <div className="preco">
+                    <div className="preco normal">
                         <label>Preço</label>
                         <input type="text" placeholder="R$ 00,00"/>
                     </div>
                 </div>
-                <div className="third">
+                <div className="third normal">
                         <label>Descrição</label>
                         <textarea type="text" placeholder="Fale brevemente sobre o prato, seus ingredientes e composição"/>
                 </div>
--- src/pages/admin/AddMeal/styles.js
+++ src/pages/admin/AddMeal/styles.js
@@ -22,11 +22,89 @@ export const Form = styled.div`
     flex-direction: column;
     gap: 32px;
 
+    h1 {
+        font-weight: 500;
+        font-size: 32px;
+        line-height: 140%;
+    }
+
+    input, textarea {
+        width: 100%;
+
+        border: none;
+        background-color: ${({ theme }) => theme.COLORS.DARK_800};
+        padding: 16px 14px 12px 14px;
+        background-color: ${({ theme }) => theme.COLORS.DARK_800};
+
+        border-radius: 8px;
+        color: ${({ theme }) => theme.COLORS.LIGHT_100};
+
+
+        &::placeholder {
+            color: ${({ theme }) => theme.COLORS.LIGHT_500};
+            font-size: 16px;
+            line-height: 100%;
+            font-family: 'Roboto', sans-serif;
+        }
+    }
+
     .first {
         display: flex;
+        align-items: flex-end;
+        justify-content: stretch;
+
+        gap: 86px;
+
+        width: 100%;
+
+        > label {
+            max-width: fit-content;
+
+
+            input {
+               display: none;
+            }
+            p {
+                color: ${({ theme }) => theme.COLORS.LIGHT_100};
+                display: flex;
+                align-items: center;
+                gap: 8px;
+
+                padding: 12px 32px;
+                background-color: ${({ theme }) => theme.COLORS.DARK_800};
+
+                border-radius: 8px;
+                cursor: pointer;
+            }
+        }
+
+        .categoria {
+            max-width: 500px;
+        }
     }
 
     .second {
         display: flex;
+        width: 100%;
+    }
+
+    .third {
+        width: 100%;
+    }
+
+    .normal {
+        width: 100%;
+
+        display: flex;
+        flex-direction:column;
+
+        gap: 12px;
+
+        label {
+            color: ${({ theme }) => theme.COLORS.LIGHT_400};
+            font-family: 'Roboto', sans-serif;
+            font-size: 16px;
+            line-height: 100%;
+        }
     }
 `

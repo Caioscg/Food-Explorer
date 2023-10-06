93837a9acab27cd18970058dbfb6b872edbd1941
Caioscg <cacazaok5@gmail.com>
2023-10-06 19:20:32 -0300

    sign in

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,3 +1,30 @@
 import { useState, useEffect, createContext, useContext } from "react";
 
 import { api } from "../services/api"
+
+export const AuthContext = createContext({})
+
+function AuthProvider({ children }) {
+    const [data, setData] = useState({})
+
+    async function SignIn({ email, password }) {
+        try {
+            const response = await api.post("/sessions", { email, password })
+            const { user, token } = response.data
+
+            localStorage.setItem("@foodexplorer:user", JSON.stringify(user))
+            localStorage.setItem("@foodexplorer:token", token)
+
+            api.defaults.headers.common['Authorization'] = `Bearer ${token}`  // aplica o token para todas no cabeçalho de todas requisições desse user
+            setData({ user, token }) // armazena no estado
+
+        } catch(error) {
+            if(error.response) {
+                alert(error.response.data.message)
+            }
+            else {
+                alert("Não foi possível entrar.")
+            }
+        }
+    }
+}
